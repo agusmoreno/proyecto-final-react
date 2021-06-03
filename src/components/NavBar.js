3c26c7f53f15dfed2a1a3e0e90b0d91709fe3cb9
agusmoreno <agusmoreno2905@gmail.com>
2021-06-03 16:57:06 -0300

    created navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import '../styles/navBar.css';
+import { Button,Navbar,Nav,Form,FormControl,NavDropdown } from 'react-bootstrap';
+
+class NavBar extends Component {
+  render() {
+    return (
+        <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">Richa</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <NavDropdown title="Catalogo" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#hairCare">Hair Care</NavDropdown.Item>
+        <NavDropdown.Item href="#facialCare">Facial Care</NavDropdown.Item>
+      </NavDropdown>
+          <Nav.Link href="#contacto">Contacto</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-primary">Search</Button>
+        </Form>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
