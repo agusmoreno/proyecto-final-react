cd6a5dd64a982dc4d9299163528047b59b57f8b6
agusmoreno <agusmoreno2905@gmail.com>
2021-10-10 13:34:46 -0300

    Update CartWidget.js

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,12 +1,15 @@
 import { Cart3 } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom'
 import { useCartContext } from "../context/CartContext"
 
 export default function App() {
   const { items } = useCartContext()
   return (
+    <Link to='/cart'>
     <div>
     <Cart3 />
     <p>{items.reduce((acum, value)=>(acum + (value.quantity)), 0)}</p>
     </div>
+    </Link>
   )
 }
